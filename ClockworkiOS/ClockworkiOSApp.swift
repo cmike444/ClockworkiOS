c685f477423e01cc02df02b94500a4bdd18b7329
Chris Mikelson <chrismikelson@gmail.com>
2021-05-06 17:38:53 -0500

    Initial Commit

---
+++ ClockworkiOS/ClockworkiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockworkiOSApp.swift
+//  ClockworkiOS
+//
+//  Created by Chris Mikelson on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ClockworkiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
